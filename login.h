a2a1a7ded6e393e3d7db4c67bae8777211631c02
KrishaPandey <83869553+KrishaPandey@users.noreply.github.com>
2025-05-18 08:41:23 +0545

    Updated login.h

--- login.h
+++ login.h
@@ -1,1 +1,44 @@
 
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include "User.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Login : public User {
+public:
+    bool loginUser() {
+        cout << "\n+----------------------------+\n"
+             << "|          LOGIN             |\n"
+             << "+----------------------------+\n";
+
+       
+        cout << "Username: ";
+        cin  >> username;
+
+        cout << "Password: ";
+        cin  >> password;
+        cout << "\n";
+
+        ifstream file("users.txt");
+        if (!file.is_open()) {
+            cout << "No users.txt found.\n";
+            return false;
+        }
+
+        string u, p, r;
+        while (file >> u >> p >> r) {
+            if (u == username && p == password) {
+                role = r;
+                return true;
+            }
+        }
+
+        cout << "Invalid username or password.\n";
+        return false;
+    }
+};
+
+#endif 
