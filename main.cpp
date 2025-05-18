068111c82cf1ca56221080dc099e460e5a3e0752
KrishaPandey <83869553+KrishaPandey@users.noreply.github.com>
2025-05-18 08:56:54 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 // main.cpp
 #include <iostream>
 #include <fstream>
-#include "Login.h"         // still included for when you bring login back
+#include "Login.h"
 #include "Registration.h"
 using namespace std;
+
 
 void showAdminMenu() {
     cout << "\n[ADMIN MENU]\n"
@@ -21,24 +22,83 @@ void showCustomerMenu() {
 }
 
 int main() {
+
     while (true) {
         cout << "\n+----------------------------+\n"
-             << "|     USER REGISTRATION      |\n"
+             << "|         MAIN MENU          |\n"
              << "+----------------------------+\n"
-             << "| 1) Register                |\n"
+             << "| 1) Register (Admins only)  |\n"
+             << "| 2) Login                   |\n"
              << "| 0) Exit                    |\n"
              << "+----------------------------+\n"
              << "Choice: ";
 
-        int choice;
-        cin >> choice;
+        int choice; cin >> choice;
+        if (choice == 0) break;
 
-        if (choice == 0) {
-            break;
+        // --- Option 1: Registration (requires Admin-login) ---
+        if (choice == 1) {
+            cout << "\nPlease login as Admin to register new users.\n";
+            Login lg;
+            if (!lg.loginUser() ||
+               (lg.getRole() != "Admin" )) {
+                cout << "? Only Admins can register.\n";
+                continue;
+            }
+
+            // Admin logged in successfully ? show Admin menu
+            while (true) {
+                showAdminMenu();
+                int a; cin >> a;
+                if (a == 0) break;                 // logout ? back to main
+                if (a == 1) {                      // register
+                    Registration rg;
+                    rg.registerUser();
+                }
+                else if (a == 2) {
+                    cout << "[Dashboard stub]\n";
+                }
+                else {
+                    cout << "? Invalid choice.\n";
+                }
+            }
         }
-        else if (choice == 1) {
-            Registration rg;
-            rg.registerUser();
+        // --- Option 2: Login (Admin or Customer) ---
+        else if (choice == 2) {
+            Login lg;
+            if (!lg.loginUser()) continue;
+
+            // branch by role
+            if (lg.getRole() == "Admin" ) {
+                while (true) {
+                    showAdminMenu();
+                    int a; cin >> a;
+                    if (a == 0) break;       
+                    if (a == 1) {
+                        Registration rg;
+                        rg.registerUser();
+                    }
+                    else if (a == 2) {
+                        cout << "[Dashboard stub]\n";
+                    }
+                    else {
+                        cout << "? Invalid choice.\n";
+                    }
+                }
+            } else {
+                // customer
+                while (true) {
+                    showCustomerMenu();
+                    int c; cin >> c;
+                    if (c == 0) break;       // logout
+                    if (c == 1) {
+                        cout << "[Customer dashboard stub]\n";
+                    }
+                    else {
+                        cout << "? Invalid choice.\n";
+                    }
+                }
+            }
         }
         else {
             cout << "? Invalid choice.\n";
@@ -48,4 +108,3 @@ int main() {
     cout << "Goodbye!\n";
     return 0;
 }
-
