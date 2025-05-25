52261e11837597ffab49cc36c1cc5af6151c0721
KrishaPandey <83869553+KrishaPandey@users.noreply.github.com>
2025-05-25 08:55:36 +0545

    Updated user.h

--- user.h
+++ user.h
@@ -10,16 +10,18 @@ protected:
     string username;
     string password;
     string role;
+    bool   locked;
 
 public:
     void setUsername(string u) { username = u; }
     void setPassword(string p) { password = p; }
     void setRole(string r)       { role = r; }
+    void setLocked(bool l)     { locked = l; } 
 
     string getUsername() { return username; }
     string getPassword() { return password; }
     string getRole()     { return role; }
+    bool   isLocked()    { return locked; }
 };
 
 #endif // USER_H
-
